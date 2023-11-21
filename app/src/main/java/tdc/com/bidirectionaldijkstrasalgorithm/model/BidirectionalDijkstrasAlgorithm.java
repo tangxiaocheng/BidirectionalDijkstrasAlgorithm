b0ee94df06d976dcebd3318fc62544634597c842
Randy <tangdengcheng@gmail.com>
2023-11-20 20:14:36 -0800

    Update BidirectionalDijkstrasAlgorithm.java

--- app/src/main/java/tdc/com/bidirectionaldijkstrasalgorithm/model/BidirectionalDijkstrasAlgorithm.java
+++ app/src/main/java/tdc/com/bidirectionaldijkstrasalgorithm/model/BidirectionalDijkstrasAlgorithm.java
@@ -3,7 +3,7 @@ package tdc.com.bidirectionaldijkstrasalgorithm.model;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Set;
-
+// rewrite in kotlin
 class BidirectionalDijkstrasAlgorithm {
 
   private Graph reverseGraph;
